e29959901c9b7dc3b699969a9d50620977f64b90
Sujita <sujita.goit@gmail.com>
2020-09-14 12:34:44 +0545

    Version Test 1.0 on 14 Sep 2020

---
+++ app/src/main/java/edu/tu/androidlecture4/Register.java
@@ -0,0 +1,21 @@
+package edu.tu.androidlecture4;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class Register extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
